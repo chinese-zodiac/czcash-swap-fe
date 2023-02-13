6d6e5dc42b452b4a0ad96eb238d798649e19b34c
Plastic Digits <plasticdigits@protonmail.com>
2023-02-13 06:13:37 -0800

    Remove BUSD from recommended base pairs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -134,7 +134,6 @@ export const FLOX = new WrappedTokenInfo(
 
 export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
-export const BUSD = new Token(ChainId.MAINNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
 export const BTCB = new Token(ChainId.MAINNET, '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', 18, 'BTCB', 'Binance BTC')
 export const USDT = new Token(ChainId.MAINNET, '0x55d398326f99059fF775485246999027B3197955', 18, 'USDT', 'Tether USD')
 export const UST = new Token(
@@ -164,7 +163,6 @@ export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
     ...WBNB_ONLY[ChainId.MAINNET],
     CZUSD,
     CZF,
-    BUSD,
     USDT,
     BTCB,
     DAI
@@ -182,18 +180,18 @@ export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: To
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...WBNB_ONLY,
-  [ChainId.MAINNET]: [BUSD, CZUSD, CZR, LRT, LSDT, GEM, DGOD]
+  [ChainId.MAINNET]: [CZUSD, CZR, LRT, LSDT, GEM, DGOD]
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   ...WBNB_ONLY,
-  [ChainId.MAINNET]: [...WBNB_ONLY[ChainId.MAINNET], CZUSD, CZF, BUSD, CZR]
+  [ChainId.MAINNET]: [...WBNB_ONLY[ChainId.MAINNET], CZUSD, CZF, CZR]
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
   [ChainId.MAINNET]: [
-    [CZUSD, BUSD],
+    [CZUSD, WBNB],
     [CZUSD, CZF]
   ]
 }
