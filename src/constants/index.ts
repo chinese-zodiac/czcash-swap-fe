ee643d59202c36edbfff278fd03318917ba66167
Plastic Digits <plasticdigits@protonmail.com>
2023-09-21 18:00:50 +0900

    Add BSR

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -157,6 +157,16 @@ export const BANDIT = new WrappedTokenInfo({
   logoURI: "https://cz.cash/images/tokens/BANDIT.png"
 },[]
 )
+export const BSR = new WrappedTokenInfo(
+  {
+    name: "Blessed Silver Rupees",
+    symbol:"BSR",
+    address:"0xe5d4A6385e131994aE325654AdAf6b4d2Ff4b4D1",
+    chainId:56,
+    decimals:18,
+    logoURI:"https://cz.cash/images/tokens/BSR.svg"
+  },[]
+)
 
 export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
@@ -206,13 +216,13 @@ export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: To
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...WBNB_ONLY,
-  [ChainId.MAINNET]: [CZUSD, CZR, CZB, LRT, LSDT, GEM, DGOD,  BANDIT]
+  [ChainId.MAINNET]: [CZUSD, CZR, CZB, LRT, LSDT, GEM, DGOD,  BANDIT, BSR]
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   ...WBNB_ONLY,
-  [ChainId.MAINNET]: [...WBNB_ONLY[ChainId.MAINNET], CZUSD, CZF, CZR, CZB, BANDIT]
+  [ChainId.MAINNET]: [...WBNB_ONLY[ChainId.MAINNET], CZUSD, CZF, CZR, CZB, BANDIT, BSR]
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
